70a3d246b80fa06341a04b3547bc6eba41a79a33
Yorgo Tabet <80336897+YorgoTabet@users.noreply.github.com>
2021-06-28 19:58:00 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+ numOfSeconds = 10;
+// Update the count down every 1 second
+function resetTimer()
+{
+   document.getElementById('reset').style.visibility="hidden";
+    var x = setInterval(function() {    
+
+        numOfSeconds = numOfSeconds - 1;
+        var hours   = Math.floor(numOfSeconds / 3600 );
+        var minutes = Math.floor(numOfSeconds % 3600 / 60);
+        var seconds = numOfSeconds % 60;
+    
+        if(minutes>0){
+            var time = [minutes, seconds].join(':');
+        }
+        else{
+            var time = seconds
+        }
+        document.getElementById("counter").innerHTML = time;
+    
+      
+      if (numOfSeconds < 0) {
+        clearInterval(x);
+        numOfSeconds = 10;
+        var audio = new Audio('splash.mp3');
+        audio.play();
+        document.getElementById('reset').style.visibility="visible";
+        document.getElementById("counter").innerHTML = "DRINK YOUR WATER,\n STAY HYDRATED!";
+      }
+    }, 1000);
+
+}
